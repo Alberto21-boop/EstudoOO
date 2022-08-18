4e57a880a81b06540fb5c3874ee21a4ff7bb0cc5
alber <alber@ALBERTO_BARBOSA>
2022-08-18 09:39:45 -0300

    Adicionar arquivos de projeto.

---
+++ EstudoOO/Program.cs
@@ -0,0 +1,13 @@
+﻿using EstudoOO;
+
+
+Carro carro = new Carro{Motorista = 3 };
+Carro carro2 = new Carro{Motorista = 21 };
+
+Console.WriteLine("motorista1 " + carro.Motorista);
+Console.WriteLine("motorista2 " + carro2.Motorista);
+
+Console.WriteLine();
+
+Console.WriteLine("motorista1 " + carro2.Motorista);
+Console.WriteLine("Motorista2 " + carro.Motorista);
